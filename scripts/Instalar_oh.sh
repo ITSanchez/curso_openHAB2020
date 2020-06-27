0b8dc440bd117f7c623dc032b95d25e26099654c
ITSanchez <gsanchez@itsanchez.com.ar>
2020-06-27 16:14:24 +0100

    Creacion Repo

---
+++ scripts/Instalar_oh.sh
@@ -0,0 +1,5 @@
+wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -
+apt -y install apt-transport-https
+echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list
+apt update
+apt -y install openhab2
