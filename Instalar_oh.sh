017ceb9c2c074729becadf79029f96838d8f2df5
ITSanchez <gsanchez@itsanchez.com.ar>
2020-06-27 16:36:58 +0100

    ITSanchez.com.ar

--- Instalar_oh.sh
+++ Instalar_oh.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -
 apt -y install apt-transport-https
 echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list
---
+++ Nano_openhab.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp ./tools/openhab.nanorc /etc
+echo 'include "/etc/openhab.nanorc"' >> /etc/nanorc 
